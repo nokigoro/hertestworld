9b8dbf9ea87381a6fadc79587f4b9eba10d32aac
matu <matu@mail>
2018-08-10 18:04:00 +0900

    jkl

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  console.log("Hello World!!");
+  console.log("Hello World!!??");
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World!!?\n');
 });
